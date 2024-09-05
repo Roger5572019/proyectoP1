96eadc4bf63a046f9704a8ab79b3e33aa9f3c473
roger557 <roger5572019@outlook.com>
2024-09-05 08:30:15 -0600

    comentarios en ejercicio2.c
    se eliminó también el ejercico1 sin extención C

--- ejercicio2.c
+++ ejercicio2.c
@@ -3,6 +3,7 @@
 
 int main()
 {
+    //declaración de variables.
     int n, numAnterior, acumulado;
     bool esImpar = false;
     numAnterior = 1;
@@ -10,34 +11,34 @@ int main()
     printf("Ingresa un número:\n");
     scanf("%i", &n);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) // Generación para las filas.
     {
         acumulado = 0;
-        for (int j = 0; j <= i; j++)
+        for (int j = 0; j <= i; j++) // Generación para las columnas.
         {
-            while (!esImpar)
+            while (!esImpar) // Verificar hasta que el número sea impar.
+
             {
                 esImpar = true;
-                if (numAnterior % 2 == 0 )
+                if (numAnterior % 2 == 0)
                 {
                     esImpar = false;
                     numAnterior++;
                 }
             }
 
-            printf("%i ", numAnterior);
-            if (i!=0 && j != i)
+            printf("%i ", numAnterior); //Imprimir el numero impar
+            if (i != 0 && j != i) // Imprimir "+" si la fila no es la primera y la ultima columna.
             {
                 printf("+ ");
             }
-            
-            acumulado += numAnterior;
-            esImpar = false;
-            numAnterior++;
 
+            acumulado += numAnterior; //sumar los números impares en acumulado
+            esImpar = false;
+            numAnterior++; 
         }
 
-        printf("= %i", acumulado);
+        printf("= %i", acumulado); //Imprimir el acumulado total
         printf("\n");
     }
 
