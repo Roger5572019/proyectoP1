bebb3a3f90fa4969f1e290105294449a99326238
JesusReyes030106 <Jesusreyesmar@outlook.com>
2024-09-07 20:15:01 -0600

    Update ejercicio3.c
    Ya se actualizo el código, es completamente funcional y adecuado a las especificaciones de la maestra

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,37 +1,59 @@
 #include <stdio.h>
+//Funcion para llenar el areglo vacio.
+void Llenar_Contendor(long long int codigo, int contendor[13]){
+    int digito, i = 12;
+    while(codigo > 0){
+        //Se separa el ultimo digito del isbn con el residuo de la divicion entre 10 del isbn.
+        digito = codigo % 10;
+        codigo = codigo / 10;
+        /*Se guarda el digito extraido del isbn, se asigna del 12 en decadencia, y
+        a que el digito que se obtiene es el ultimo, esto para seguir un orden.*/
+        contendor[i] = digito;
+        i--;
+    }
+}
 
-int main(){
-    //Se delcaran las siguiente variables como long long int para que puedan soportar digitos de 12 numeros.
-    long long int isbn, isbn2;
-    int  contador = 1, sumatoria = 0, resultado, digito, digitoVerificador;
-    printf("Ingresa los primeros 12 digitos del ISBN: ");
-    // se declara que el scandf tomara un valor "%lld" long long int.
-    scanf("%lld", &isbn);
-
-    isbn2 = isbn;
-    while(isbn2>0){
-        // Se separa el ultimo digito del isbn.
-        digito = isbn2 % 10;
+//Funcion para validar el areglo que se lleno con la funcion anterior.
+void ValidarISBN(int contenedor[13]){
+    int digitoValidador = contenedor[12], contador = 2, sumatoria = 0, resultado;
+    for(int i = 0; i <= 11; i++){
         if(contador % 2 == 0){
-            //Si el contador es inpar el digito se multiplica por 1 y se suma periodicamente a la variable sumatoria.
-            sumatoria += (digito * 1);
+            /*Se utiliza un contador para saber que numero esta en una posicion par e inpar,
+            esto para multiplicar por 3 los numeros pares y los impares por 1, ademas se sumar esta multiplicacion*/
+            sumatoria += (contenedor[i] * 1);
             contador = 1;
         }
         else{
-            //Si el contador es par el digito se multiplica por 3 y se suma peridodicamente a la variable sumatoria.
-            sumatoria += (digito * 3);
+            sumatoria += (contenedor[i] * 3);
             contador = 2;
         }
-        // Se divide el isbn entre 10 para pasar a la siguiente cifra del numero.
-        isbn2 = isbn2 / 10;
     }
-    //Se valida el digito validador y se muestra.
-    resultado = sumatoria % 10;
-    digitoVerificador = 10 - resultado;
-    if(digitoVerificador == 10){
-        //En el caso de que el digito verificador sea 10, este automaticamente se convierte en 0, ya que el codigo verificador es del 0 - 9.
-        digitoVerificador = 0;
+    resultado = 10 - (sumatoria % 10);
+    /*Se valida el isbn, en este caso se realiza tambien una condicion, si el digito validador obtenido es 10, 
+    automaticamente cambia a 0, ya que el digito validador va de 9 - 0*/
+    if(resultado == 10){
+        resultado = 0;
     }
-    printf("El digito verificador es: %d", digitoVerificador);
-    return 0;
+    //Si el digito obtenido es igual al digito ingresado el isbn es valido de lo contrario es invalido.
+    if(digitoValidador == resultado){
+        printf("El ISBN es valido");
+    }
+    else{
+        printf("El isbn es Invalido");
+    }
+
+}
+
+int main(){
+
+    long long int isbn;
+    int contenedor[13];
+
+    printf("Ingresa el codigo ISBN: ");
+    scanf("%lld", &isbn);
+    //Se llama a las funciones, ademas de mandar los parametros.
+    Llenar_Contendor(isbn, contenedor);
+    ValidarISBN(contenedor);
+
+    return 0;    
 }
